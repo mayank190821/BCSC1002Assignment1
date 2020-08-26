7691668d06b7346c946642052467bd1309aa2004
mayank190821 <mayank.123.2000@gmail.com>
2020-08-26 21:36:01 +0530

    add switch case and layout in Front Desk class.

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -6,8 +6,30 @@
  * */
 package execution;
 
+import java.util.Scanner;
+
 public class FrontDesk {
     public static void main(String[] args) {
-
+        int choice;
+        System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+        System.out.println("How may I help you today?");
+        System.out.println("1. Issue a new book for me.");
+        System.out.println("2. Return a previously issues book for me.");
+        System.out.println("3. Show me all my issues books.");
+        System.out.println("4. Exit.");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your choice ");
+        choice = scan.nextInt();
+        scan.close();
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4 :
+                break;
+        }
     }
 }
